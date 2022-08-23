6a65d4d2bfb649b23a7f5e7955a4fa566acd92d5
Nicholas Natsoulas <nrn22@cornell.edu>
2022-08-23 00:28:15 -0400

    Update Earthrfunc.m

--- 6DOF_RCS_Sim/Earthrfunc.m
+++ 6DOF_RCS_Sim/Earthrfunc.m
@@ -1,4 +1,8 @@
 function r = Earthrfunc(t,IC)
+%Please replace Earth with desired body in function title,
+%and keplerian elements with desired body's in the code below in order to include another body
+%don't forget to update dynamics with gravity from the newly added body too
+%should be orbit_attitude_numint.m or something similar for dynamics
     a = (1.495468303960526E+08)*1000; %km 2 meters
     e = 1.699803590732099E-02; % 
     I = 4.787963974336582E-03*pi/180;
